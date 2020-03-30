af08e72c836a53de2f3804d128746c30ce6126d8
GentoMedia <53331146+GentoMedia@users.noreply.github.com>
2020-03-31 03:10:28 +0900

    Adjusted Comments
    Adjusted the comments so they are easier to read. Moved them from being tabbed inline to preceding the line they explain.

--- PythonPart1CheatSheet.py
+++ PythonPart1CheatSheet.py
@@ -18,67 +18,91 @@
 #   Variable Types   #
 ######################
 
-this_is_var = "This is a Variable"			# variables store values by name
-											# variable name can be a combination of
-											# letters, numbers, or _
-											# uses snake_case and can't start with number
+# variables store values by name
+this_is_var = "This is a Variable"
+# variable name can be a combination of
+# letters, numbers, or _
+# uses snake_case and can't start with number
 
-int_var = 32								# integer
-float_var = 64.2							# number with decimal
-str_var = "This is not a number"			# string of characters
-bool_var = True								# boolean - must be title case (True or False)
+# integer
+int_var = 32
+# number with decimal
+float_var = 64.2
+# string of characters
+str_var = "This is not a number"
+# boolean - must be title case (True or False)
+bool_var = True
 
-int_var_to_str = str(12)					# changes to a string
-str_to_float = float("6.4")					# changes to a float
-float_to_int = int(1.6)						# changes to int
+# changes to a string
+int_var_to_str = str(12)
+# changes to a float
+str_to_float = float("6.4")
+# changes to int
+float_to_int = int(1.6)
 
 
 #########################
 #   Simple Operations   #
 #########################
 
-int_var = 32 / 8							# division - now eqals 4
-float_var = 64.2 + 3						# addition - now equals 67.2
-int_var = 8 * 2								# multiplication - now equals 16
-float_var = 67.2 - 7						# subtraction - now equals 60.2
+# division
+int_var = 32 / 8
+# addition
+float_var = 64.2 + 3
+# multiplication
+int_var = 8 * 2
+# subtraction
+float_var = 67.2 - 7
 
-int_var = (16 / 2) + 8						# parentheses performed first
+# parentheses performed first
+int_var = (16 / 2) + 8
 
-int_var = 16**2								# exponent - now equals 256
-int_var = 256**(1/2)						# exponent - now equals 16
+# exponent
+int_var = 16**2	
+int_var = 256**(1/2)
 
-int_var = 16 // 5							# floor division (drops remainder) - now equals 3
-int_var = 3 % 2								# modulo (returns remainder) - now equals 1
+# floor division (drops remainder)
+int_var = 16 // 5
+# modulo (returns remainder) - now equals 1
+int_var = 3 % 2
 
-int_var += 1								# adds the right to the left and saves it to the variable
-											# - now equals 2
-											# can use -= *= /= %= //= **= for different operations
+# adds the right to the left and saves it to the variable
+int_var += 1
+# now equals 2
+# can use -= *= /= %= //= **= for different operations
 
 
 ############################
 #   Playing with Strings   #
 ############################
 
-print(str_var)								# outputs string to console (drops outer quotations)
-new_str = input("Type something here: ")	# takes input from the user(as string)
+# takes input from the user(as string)
+new_str = input("Type something here: ")
+# outputs string to console (drops outer quotations)
 print(new_str)
 
-str_var = 'This is a string'				# single ' or " can also be used for strings
+# single ' or " can also be used for strings
+str_var = 'This is a string'
 print(new_str)
 
-str_var = "This \nis \na \nstring"			# \n creates a new line for the text
+# \n creates a new line for the text
+str_var = "This \nis \na \nstring"
 print(str_var)
 
-str_var = 'This is Gento\'s text'			# \ allows use of function characters within string
+# \ allows use of function characters within string
+str_var = 'This is Gento\'s text'
 print(str_var)
 
-str_var = "This " + "is a string"			# strings can be added to make one string
+# strings can be added to make one string
+str_var = "This " + "is a string"
 print(str_var)
 
-str_var = "This is the number " + str(12)	# numbers need to be converted to strings to add
+# numbers need to be converted to strings to add
+str_var = "This is the number " + str(12)
 print(str_var)
 
-str_var = "string" * 3						# can multiple string by an integer to repeat
+# can multiple string by an integer to repeat
+str_var = "string" * 3
 print(str_var)
 
 
@@ -86,30 +110,44 @@ print(str_var)
 #   Comparisons and Conditionals   #
 ####################################
 
-1 == 1										# == returns True if both sides are equal
-1 != 1										# != returns True is not equal
+# returns True if both sides are equal
+1 == 1
+# != returns True is not equal
+1 != 1
 
-2 > 1										# > returns True if left side is greater
-2 >= 1										# >= returns True if left side is greater or equal
-1 < 2										# < returns True if left side is lesser
-1 <= 2										# <= returns True if left side is lesser or equal
+# > returns True if left side is greater
+2 > 1
+# >= returns True if left side is greater or equal
+2 >= 1
+# < returns True if left side is lesser
+1 < 2
+# <= returns True if left side is lesser or equal
+1 <= 2
 
-1 == 1 and 2 == 2							# and check is True only if both sides True
-1 == 2 or 1 == 1							# or check is True if at least one side is True
-not 1 == 2									# not check is True statement after is False
+# True only if both sides True
+1 == 1 and 2 == 2
+# True if at least one side is True
+1 == 2 or 1 == 1
+# True if statement after is False
+not 1 == 2
 
-if 1 == 1:									# checks statement and runs if True (needs 
-	print("This is True")					# inside needs tab - tabs indicate what's inside
-	
-if 1 == 2:									# if False won't run what's inside
+# checks statement and runs if True
+if 1 == 1:
+	print("This is True")
+
+# if False won't run what's inside
+if 1 == 2:
 	print("You won't see this in execute")
-elif 1 == 3:								# elif will run if previous if / elif is False
-	print("You won't see this either")		# - and the check is True (must follow if / elif)
-	if 1 == 1:								# these checks and statements can be nested
+# elif will run if previous if / elif is False and the check is True (must follow if / elif)
+elif 1 == 3:
+	print("You won't see this either")
+	# these checks and statements can be nested
+	if 1 == 1:
 		print("This is a nested if")
-else:										# else will run if previous if / elif is False
-	print("This is from an else statement")	# - (must be at end of if / elif chain)
-
+# will run if previous if / elif is False
+else:
+	print("This is from an else statement")
+# else will run if previous if / elif is False (must be at end of if / elif chain)
 
 #############################
 #   P E M D A S C N A O A   #
@@ -132,14 +170,19 @@ else:										# else will run if previous if / elif is False
 #   Lists and Loops   #
 #######################
 
-this_list = [1, 2, 3, 4]					# lists are made with brackets
-int_var = this_list [0]						# calls in specific value in that index
-											# index starts at 0 - this_list index is 0-3
+# lists are made with brackets
+this_list = [1, 2, 3, 4]
+# calls in specific value in that index (starts at 0)
+int_var = this_list [0]
+# this_list index is 0-3
 
-for item in this_list:						# for cycles through a list and preforms what"s inside
+# for cycles through a list and preforms what"s inside
+for item in this_list:
 	print(item)
 
+# while loop runs what's inside repeatedly if True
 counter = 0
-while counter < 3:							# while loop runs what's inside repeatedly if True
-	print("In a loop")						# - this prints the string 3 times
+while counter < 3:
+	print("In a loop")
 	counter += 1
+# this prints the string 3 times
